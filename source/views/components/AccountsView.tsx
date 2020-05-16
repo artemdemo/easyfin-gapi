305db7b6e4273a9ecbe8755d92a1f9ff44af7649
Artem Demo <artemdemo@gmail.com>
2020-05-16 13:06:33 +0300

    added plan comment in AccountsList

--- source/views/components/AccountsView.tsx
+++ source/views/components/AccountsView.tsx
@@ -13,6 +13,13 @@ class AccountsView extends React.PureComponent<TProps, TState> {
     };
 
     componentDidMount() {
+        // ToDo: I'll need to use account list at least in 2 places:
+        //  * here (Account list page)
+        //  * while adding a transaction
+        //  I don't like the idea that I'll need to request it each time.
+        //  Let's say I'll use reducer with `accounts` property, which default value will be `null`.
+        //  This way I'll know that list has been never requested and will do it also in EditTransaction view.
+        //  But by default accounts list on each visit will be requested only here - in AccountsList view.
         loadAccounts()
             .then(accounts => this.setState({ accounts }));
     }
