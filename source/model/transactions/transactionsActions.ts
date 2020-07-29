d4ccdbe548b36b7e6f03e663955117f199d9cd80
Artem Demo <artemdemo@gmail.com>
2020-07-29 18:09:28 +0300

    transaction actions

---
+++ source/model/transactions/transactionsActions.ts
@@ -0,0 +1,26 @@
+import { createAction } from "redux-actions";
+import GTransactionRow from "../../google-api/GTransactionRow";
+
+export type TLoadTransactionsPayload = undefined;
+export type TLoadTransactions = (payload?: TLoadTransactionsPayload) => void;
+export const loadTransactions = createAction('LOAD_TRANSACTIONS');
+export const transactionsLoaded = createAction('TRANSACTIONS_LOADED');
+export const transactionsLoadingError = createAction('TRANSACTIONS_LOADING_ERROR');
+
+export type TDeleteTransactionPayload = GTransactionRow;
+export type TDeleteTransaction = (payload: TDeleteTransactionPayload) => void;
+export const deleteTransaction = createAction('DELETE_TRANSACTION');
+export const transactionDeleted = createAction('TRANSACTION_DELETED');
+export const transactionDeletingError = createAction('TRANSACTION_DELETING_ERROR');
+
+export type TCreateTransactionPayload = GTransactionRow;
+export type TCreateTransaction = (payload: TCreateTransactionPayload) => void;
+export const createTransaction = createAction('CREATE_TRANSACTION');
+export const transactionCreated = createAction('TRANSACTION_CREATED');
+export const transactionCreatingError = createAction('TRANSACTION_CREATING_ERROR');
+
+export type TUpdateTransactionPayload = GTransactionRow;
+export type TUpdateTransaction = (payload: TUpdateTransactionPayload) => void;
+export const updateTransaction = createAction('UPDATE_TRANSACTION');
+export const transactionUpdated = createAction('TRANSACTION_UPDATED');
+export const transactionUpdatingError = createAction('TRANSACTION_UPDATING_ERROR');
