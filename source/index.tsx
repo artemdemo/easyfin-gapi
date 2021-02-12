69a328b3eb63deb62dd5fc4d8488621cd9cd2188
Artem Demo <artemdemo@gmail.com>
2021-02-12 14:40:24 +0200

    reorg webpack

--- source/index.tsx
+++ source/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import MainRoutes from './routing/MainRoutes';
 import store from './store';
 import history from './history';
 
-import './styles/general.less';
+import './styles/general.css';
 
 render(
-    <MainRoutes
-        store={store}
-        history={history}
-    />,
-    document.getElementById('app'),
+  <MainRoutes
+    store={store}
+    history={history}
+  />,
+  document.getElementById('app'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const {
+  IgnorePlugin,
+  DefinePlugin,
+} = require('webpack');
+
 const packageFile = require('./package.json');
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -10,13 +17,96 @@ const configOptions = {
   isProduction,
 };
 
-const webpackConfig = isProduction ?
-  require('./webpack/webpack.prod')(configOptions) :
-  require('./webpack/webpack.dev')(configOptions);
+const path = require('path');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-if (process.argv.indexOf('--json') === -1) {
-  // eslint-disable-next-line no-console
-  console.log('\n', ' ❤ isProduction:', isProduction, '\n');
+module.exports = (env, argv) => {
+  return {
+    entry: path.join(__dirname, 'source', 'index.tsx'),
+    output: {
+      // path: path.resolve(__dirname, 'dist'),
+      path: `${process.cwd()}/${configOptions.buildFolder}`,
+      filename: configOptions.isProduction ?
+        './js/[name]-[chunkhash].js' :
+        './js/[name].js',
+      chunkFilename: configOptions.isProduction ?
+        './js/[id].chunk-[chunkhash].js' :
+        './js/[id].chunk.js',
+      publicPath: '/',
+    },
+    devServer: {
+      // open: true,
+      host: 'easyfin.local',
+      port: 8080,
+      // clientLogLevel: 'silent',
+      contentBase: `${configOptions.buildFolder}/`,
+      historyApiFallback: true,
+      hot: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.([tj])sx?$/,
+          include: path.resolve(__dirname, 'source'),
+          exclude: /node_modules/,
+          use: [{
+            loader: 'ts-loader',
+            options: {},
+          }],
+        },
+        {
+          test: /\.css$/i,
+          include: path.resolve(__dirname, 'source'),
+          exclude: /node_modules/,
+          use: [
+            'style-loader',
+            // {
+            //   loader: MiniCssExtractPlugin.loader,
+            //   options: {
+            //     hmr: argv.mode === 'development'
+            //   }
+            // },
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1
+              }
+            },
+            'postcss-loader'
+          ]
+        },
+      ]
+    },
+    plugins: [
+      // Defining global ENV variable
+      // Useful for some age cases, when you need explicitly know whether you're in development or not
+      // For example, when you want to log out something only in development mode
+      // and don't want to delete this code in production, just want to deactivate it then.
+      new DefinePlugin({
+        ENV: {
+          production: configOptions.isProduction,
+          clientId: JSON.stringify(configOptions.clientId),
+          apiKey: JSON.stringify(configOptions.apiKey),
+        },
+      }),
+
+      new HtmlWebpackPlugin({
+        template: './source/index.ejs',
+        filename: './index.html',
+        appVersion: configOptions.appVersion,
+      }),
+
+      new CleanWebpackPlugin({
+        verbose: true,
+        dry: false,
+        cleanOnceBeforeBuildPatterns: [
+          '**/*',
+          '!.gitignore',
+        ],
+      }),
+    ],
+  }
 }
-
-module.exports = webpackConfig;
