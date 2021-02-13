4fc704c2eeaf06b7938adb58345213b606fa8593
Artem Demo <artemdemo@gmail.com>
2021-02-13 16:53:28 +0200

    reorg error handler

--- source/components/ErrorHandler/ErrorHandler.tsx
+++ source/components/ErrorHandler/ErrorHandler.tsx
@@ -7,42 +7,42 @@ import InfoPanel, {EInfoAppearance} from '../InfoPanel/InfoPanel';
 type TProps = {};
 
 type TState = {
-    error: Error|null;
+  error: Error | null;
 };
 
 class ErrorHandler extends React.PureComponent<TProps, TState> {
-    state = {
-        error: null,
-    }
+  state = {
+    error: null,
+  }
 
-    static getDerivedStateFromError(error) {
-        return {
-            error,
-        };
-    }
+  static getDerivedStateFromError(error) {
+    return {
+      error,
+    };
+  }
 
-    componentDidCatch(error, info) {}
+  componentDidCatch(error, info) {}
 
-    render() {
-        if (this.state.error) {
-            const { message } = this.state.error as unknown as Error;
-            return (
-                <InfoPanel
-                    title={(
-                        <>
-                            <FontAwesomeIcon icon={faExclamationTriangle} />
-                            &nbsp;
-                            {t('Something broke')}
-                        </>
-                    )}
-                    appearance={EInfoAppearance.error}
-                >
-                    {message}
-                </InfoPanel>
-            );
-        }
-        return this.props.children;
+  render() {
+    if (this.state.error) {
+      const {message} = this.state.error as unknown as Error;
+      return (
+        <InfoPanel
+          title={(
+            <>
+              <FontAwesomeIcon icon={faExclamationTriangle}/>
+              &nbsp;
+              {t('Something broke')}
+            </>
+          )}
+          appearance={EInfoAppearance.error}
+        >
+          {message}
+        </InfoPanel>
+      );
     }
+    return this.props.children;
+  }
 }
 
 export default ErrorHandler;
