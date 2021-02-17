50d608abd41c46d0acca04112519831d56e73103
Artem Demo <artemdemo@gmail.com>
2021-02-17 21:33:28 +0200

    moved routes around

--- source/index.tsx
+++ source/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import MainRoutes from './routing/MainRoutes';
 import store from './store';
 import history from './history';
-
+import AppView from './views/AppView';
 import './styles/general.css';
 
 render(
-  <MainRoutes
-    store={store}
-    history={history}
-  />,
+  <Provider store={store}>
+    <Router history={history}>
+      <AppView>
+        <MainRoutes />
+      </AppView>
+    </Router>
+  </Provider>,
   document.getElementById('app'),
 );
--- source/routing/MainRoutes.tsx
+++ source/routing/MainRoutes.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import {Router, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import * as routes from './routes';
-import AppView from '../views/AppView';
 import MainView from '../views/MainView';
 import LoginView from '../views/LoginView';
 import EditTransaction from '../views/EditTransactionView';
@@ -15,32 +13,21 @@ const TransactionsView = React.lazy(() => import('../views/TransactionsView'));
 const AccountsView = React.lazy(() => import('../views/AccountsView'));
 const CategoriesView = React.lazy(() => import('../views/CategoriesView'));
 
-type TProps = {
-  store: any;
-  history: any;
-};
-
-const MainRoutes = (props: TProps) => (
-  <Provider store={props.store}>
-    <Router history={props.history}>
-      <AppView>
-        <React.Suspense fallback={<ViewLoading />}>
-          <Route path={routes.main()} component={MainView} exact/>
-          <Route path={routes.login()} component={LoginView}/>
-          <Route path={routes.transactions()} component={TransactionsView} exact/>
-          <Route path={routes.transactions.new()} component={EditTransaction}/>
-          <Route path={routes.transactions.edit()} component={EditTransaction}/>
-          <Route path={routes.accounts()} component={AccountsView} exact/>
-          <Route path={routes.accounts.new()} component={EditAccountView}/>
-          <Route path={routes.accounts.edit()} component={EditAccountView}/>
-          <Route path={routes.categories()} component={CategoriesView} exact/>
-          <Route path={routes.categories.new()} component={EditCategoryView}/>
-          <Route path={routes.categories.edit()} component={EditCategoryView}/>
-          <Route path={routes.settings()} component={SettingsView}/>
-        </React.Suspense>
-      </AppView>
-    </Router>
-  </Provider>
+const MainRoutes: React.FC = () => (
+  <React.Suspense fallback={<ViewLoading />}>
+    <Route path={routes.main()} component={MainView} exact/>
+    <Route path={routes.login()} component={LoginView}/>
+    <Route path={routes.transactions()} component={TransactionsView} exact/>
+    <Route path={routes.transactions.new()} component={EditTransaction}/>
+    <Route path={routes.transactions.edit()} component={EditTransaction}/>
+    <Route path={routes.accounts()} component={AccountsView} exact/>
+    <Route path={routes.accounts.new()} component={EditAccountView}/>
+    <Route path={routes.accounts.edit()} component={EditAccountView}/>
+    <Route path={routes.categories()} component={CategoriesView} exact/>
+    <Route path={routes.categories.new()} component={EditCategoryView}/>
+    <Route path={routes.categories.edit()} component={EditCategoryView}/>
+    <Route path={routes.settings()} component={SettingsView}/>
+  </React.Suspense>
 );
 
 export default MainRoutes;
